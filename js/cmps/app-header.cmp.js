5a51e60786182beae0fadba82ca0110595690cf8
sowhat1234 <emiliu1234e@gmail.com>
2022-03-04 00:48:23 +0200

    home page design

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,23 +1,35 @@
 export default {
     template: `
-        <header class="main-header space-between">
+      	<header class="light-gray">
+		<div class="header-inner">
+			<div class="logo">
+				<img src="" alt="">
+			</div>
+			<div class="menu">
+				<ul>
+					<li><router-link to="/"><span>Home</span></router-link></li>
+					<li><router-link to="/mail"><span>Mail</span></router-link></li>
+					<li><router-link to="/about"><span>About</span></router-link></li>
+					<li><router-link to="/notes"><span>Notes</span></router-link></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+        <!-- <header class="main-header space-between">
             <div class="logo">
                 <h2>App-sus</h2>
             </div>
             <nav class="nav-bar">
-                <router-link to="/"><span>Home</span></router-link>|
-                <router-link to="/mail"><span>Mail</span></router-link>|
-                <router-link to="/about"><span>About</span></router-link>|
-                <router-link to="/notes"><span>Notes</span></router-link>
+              
             </nav>
 
-        </header>
+        </header> -->
     `,
     components: {},
     data() {
         return {}
     },
-    created() { },
+    created() {},
     methods: {},
     computed: {},
 
--- js/views/home-page.cmp.js
+++ js/views/home-page.cmp.js
@@ -1,7 +1,26 @@
 export default {
     template: `
-        <section class="about-page">
-            home page
-        </section>
+        <section class="home common">
+	<div class="shape">
+		<img class="shape1" src="./img/google-play.svg" alt="">
+		<img class="shape2" src="./img/google-maps.svg" alt="">
+		<img class="shape3" src="./img/google-drive.svg" alt="">
+		<img class="shape4" src="./img/google-keep.svg" alt="">
+		<img class="shape5" src="./img/google.svg" alt="">
+	</div>
+
+	<div class="home-inner">
+		<div class="text">
+			<h5>Welcome To  App-sus</h5>
+			<h1>your go to app</h1>
+			<p>Pharetra Beatae accumsan voluptaes. Anim vero consequat erat ac eleifend dictumst! Adipisci expedita? Nullam tortor voluptatibus dapibus tortor pariatur do.</p>
+			<router-link to="/about" class="btn">Learn more about us.</router-link>
+		</div>
+		<div class="img">
+			<img src="" alt="">
+		</div>
+	</div>
+
+</section>
     `,
 }
